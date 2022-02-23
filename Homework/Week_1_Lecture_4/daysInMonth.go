fe9647afb187f7d6283176ff41de7a3ddc72646f
nakata7193 <atanas925@gmail.com>
2022-02-23 13:10:46 +0200

    done

--- Homework/Week_1_Lecture_4/daysInMonth.go
+++ Homework/Week_1_Lecture_4/daysInMonth.go
@@ -6,49 +6,37 @@ func daysInMonth(month int, year int) (int, bool) {
 	switch month {
 	case 1:
 		return 31, true
-		break
 	case 2:
 		if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
 			return 29, true
 		} else {
 			return 28, true
 		}
-		break
 	case 3:
 		return 31, true
-		break
 	case 4:
 		return 30, true
-		break
 	case 5:
 		return 31, true
-		break
 	case 6:
 		return 30, true
-		break
 	case 7:
 		return 31, true
-		break
 	case 8:
 		return 31, true
-		break
 	case 9:
 		return 30, true
-		break
 	case 10:
 		return 31, true
-		break
 	case 11:
 		return 30, true
-		break
 	case 12:
 		return 31, true
-		break
 	default:
-		return 0, false
+		return 0, false 
 	}
-} //missing return??
+}
 
 func main() {
-	fmt.Println(daysInMonth(6, 1992))
+	fmt.Print(daysInMonth(16, 2020))
 }
