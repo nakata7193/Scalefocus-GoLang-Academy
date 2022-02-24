266be51915b5291674ab38288f05082ea8f19c4b
nakata7193 <atanas925@gmail.com>
2022-02-24 03:52:18 +0200

    still not workin idk

--- Homework/Week_2_Lecture_5/main.go
+++ Homework/Week_2_Lecture_5/main.go
@@ -1,31 +1,54 @@
 package main
 
-import "github.com/nakata7193/Week_2_Lecture_5/cards"
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type cardVal = int
+
+
+const (
+	two cardVal = iota + 2
+	three
+	four
+	five
+	six
+	seven
+	eight
+	nine
+	ten
+	J
+	D
+	K
+	A
+)
+
+type cardSuit = int
+
+const (
+	club cardSuit = iota + 1
+	diamond
+	heart
+	spade
+)
+
+type Card struct {
+	CardVal  cardVal
+	CardSuit cardSuit
+}
+
+cardOne := Card{4,3}
 
 func compareCards(cardOne Card, cardTwo Card) int {
 	//checks if suits are equal
-	if cardOne == cardTwo {
-		if cardOne == cardTwo {
-			return 0
-		} else if cardOne < cardTwo {
-			return 1
-		} else {
-			return -1
-		}
+	if reflect.DeepEqual(cardOne, cardTwo) {
+		return 0
+	} else if cardOne < cardTwo {
+		return 1
 	}
-	//checks if suits are not equal
-	if cardOne != cardTwo {
-		if cardOne < cardTwo {
-			return 1
-		} else {
-			return -1
-		}
-	}
-	return -10
 }
 
 func main() {
-	fmt.Print(compareCards(7, 1, 7, 2))
+	fmt.Print(compareCards())
 }
-
