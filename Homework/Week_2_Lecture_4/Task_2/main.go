b5d4713a864ccf95306a7ef8d9e45b6b57ea2380
nakata7193 <atanas925@gmail.com>
2022-03-09 14:34:21 +0200

    fix

---
+++ Homework/Week_2_Lecture_4/Task_2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func daysInMonth(month int, year int) (int, bool) {
+	switch month {
+	case 1:
+
+		return 31, true
+	case 2:
+		if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+			return 29, true
+		} else {
+			return 28, true
+		}
+	case 3:
+		return 31, true
+	case 4:
+		return 30, true
+	case 5:
+		return 31, true
+	case 6:
+		return 30, true
+	case 7:
+		return 31, true
+	case 8:
+		return 31, true
+	case 9:
+		return 30, true
+	case 10:
+		return 31, true
+	case 11:
+		return 30, true
+	case 12:
+		return 31, true
+	default:
+		return 0, false 
+	}
+}
+
+func main() {
+	fmt.Print(daysInMonth(16, 2020))
+}
