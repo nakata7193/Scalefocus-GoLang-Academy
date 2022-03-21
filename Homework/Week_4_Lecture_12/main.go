1ac85625bf285dc7ede29c7f35185d10680abf12
nakata7193 <atanas925@gmail.com>
2022-03-21 15:49:05 +0200

    still have to finish

--- Homework/Week_4_Lecture_12/main.go
+++ Homework/Week_4_Lecture_12/main.go
@@ -6,7 +6,28 @@ import (
 )
 
 func generateThrottled(data string, bufferLimit int, clearInterval time.Duration) <-chan string {
-	return nil
+	channel := make(chan string, bufferLimit)
+	handleChannel := make(chan string)
+	go func() {
+		for {
+			handleChannel <- data
+			select {
+			case n := <-handleChannel:
+				channel <- n
+			case <-timeAfter(clearInterval,channel):
+
+		}
+	}()
+	return channel
+}
+
+func timeAfter(after time.Duration,ch chan string) chan time.Time {
+	done := make(chan time.Time)
+	go func() {
+		time.Sleep(after)
+		
+	}()
+	return done
 }
 
 func main() {
