659c3440fcb32c2b65dcfdd09a4852c406efd19a
nakata7193 <atanas925@gmail.com>
2022-03-24 21:44:54 +0200

    .

--- Homework/Week_5_Lecture_13/main.go
+++ Homework/Week_5_Lecture_13/main.go
@@ -11,20 +11,25 @@ type BufferedContext struct {
 }
 
 func NewBufferedContext(timeout time.Duration, bufferSize int) *BufferedContext {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout) /*Implement the rest */
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	/*Implement the rest */
 	return nil
 
 }
 
 func (bc *BufferedContext) Done() <-chan struct{} {
 	/* This function will serve in place of the oriignal context */
-	/* make it so that the result channel gets closed in one of the to cases;
-	a) the emebdded context times out        b) the buffer gets filled    */
+	/*
+		make it so that the result channel gets closed in one of the to cases;
+		a) the emebdded context times out
+		b) the buffer gets filled
+	*/
 	return nil
 }
 
-func (bc *BufferedContext) Run(fn func(context.Context, chanstring)) {
-	/* This function serves for executing the test */ /* Implement the rest */
+func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
+	/* This function serves for executing the test */
+	/* Implement the rest */
 }
 
 func main() {
