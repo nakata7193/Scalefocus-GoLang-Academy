49772ca2a72067276f5ce4d0d227a76f2f81f82e
nakata7193 <atanas925@gmail.com>
2022-03-27 13:57:00 +0300

    ,

--- Homework/Week_5_Lecture_13/main.go
+++ Homework/Week_5_Lecture_13/main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
 type BufferedContext struct {
-	context.Context
-	/* Add other fields you might need */
+	ctx    context.Context
+	cancel context.CancelFunc
+	timeout time.Duration
+	buffersize int
 }
 
 func NewBufferedContext(timeout time.Duration, bufferSize int) *BufferedContext {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	/*Implement the rest */
-	return nil
-
+	defer cancel()
+	return &BufferedContext{ctx: ctx, cancel: cancel,timeout: timeout,buffersize: bufferSize}
 }
 
 func (bc *BufferedContext) Done() <-chan struct{} {
-	/* This function will serve in place of the oriignal context */
-	/*
-		make it so that the result channel gets closed in one of the to cases;
-		a) the emebdded context times out
-		b) the buffer gets filled
-	*/
-	return nil
+	done := make(chan BufferedContext)
+	select{
+	case<-bc.timeout:
+		
+	}
 }
+
+/* This function will serve in place of the oriignal context */
+/*
+	make it so that the result channel gets closed in one of the to cases;
+	a) the emebdded context times out
+	b) the buffer gets filled
+*/
 
 func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
 	/* This function serves for executing the test */
@@ -33,5 +40,16 @@ func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
 }
 
 func main() {
-
+	ctx := NewBufferedContext(time.Second, 10)
+	ctx.Run(func(ctx context.Context, buffer chan string) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case buffer <- "bar":
+				time.Sleep(time.Millisecond * 200) // try different values here
+				fmt.Println("bar")
+			}
+		}
+	})
 }
